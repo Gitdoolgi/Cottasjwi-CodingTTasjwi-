3dd87dd49f01faf427d9a3f534ced3a7247694ea
swansea <leesuwan89@gmail.com>
2024-06-23 20:54:07 +0900

    repository로 변경

--- src/event/JoinEvent.java
+++ src/event/JoinEvent.java
@@ -1,24 +1,21 @@
 package event;
 
-import dbutil.MaraiConnection;
 import domain.Member;
+import repository.MemberRepository;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 public class JoinEvent implements ActionListener {
-  private Connection con = MaraiConnection.getInstance().getConnection();
 
   private List<JTextField> textFieldList;
+  private MemberRepository memberRepository;
 
   public JoinEvent(List<JTextField> textFieldList) {
     this.textFieldList = textFieldList;
+    this.memberRepository = new MemberRepository();
   }
 
   @Override
@@ -43,64 +40,16 @@ public class JoinEvent implements ActionListener {
       return;
     }
 
-    int result = insertMember(new Member(idValue, passwordValue, nameValue, phoneNumValue, addressValue));
+    int result = memberRepository.insertMember(new Member(idValue, passwordValue, nameValue, phoneNumValue, addressValue));
     if (result == 0) {
       JOptionPane.showMessageDialog(null, "아이디를 변경해주세요.", "Message", JOptionPane.ERROR_MESSAGE);
 
-      int lastNo = getLastAutoincrement();
-      resetAutoincrement(lastNo);
+      int lastNo = memberRepository.getLastAutoincrement();
+      memberRepository.resetAutoincrement(lastNo);
 
-      System.out.println(getLastAutoincrement() + " 실패");
       return;
     }
     System.out.println("회원가입 성공");
-    System.out.println(getLastAutoincrement() + " 성공");
-  }
-
-  private void resetAutoincrement(int lastNo) {
-    PreparedStatement pstmt = null;
-    String sql = "ALTER TABLE tspoon_member AUTO_INCREMENT = ?";
-    try {
-      pstmt = con.prepareStatement(sql);
-      pstmt.setInt(1, lastNo);
-      pstmt.execute();
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-  }
-
-  private int getLastAutoincrement() {
-    PreparedStatement pstmt = null;
-    ResultSet rs = null;
-    String sql = "select * from tspoon_member order by tspoon_no desc limit 2";
-    int lastNo = 0;
-    try {
-      pstmt = con.prepareStatement(sql);
-      rs = pstmt.executeQuery();
-      rs.last();
-      lastNo = rs.getInt(1);
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-    return lastNo;
-  }
-
-  private int insertMember(Member member) {
-    String sql = "insert into tspoon_member(id, password, name, phone_num, address, join_date) values (?,?,?,?,?,now())";
-    int result = 0;
-    try {
-      PreparedStatement pstmt = con.prepareStatement(sql);
-      pstmt.setString(1, member.getId());
-      pstmt.setString(2, member.getPassword());
-      pstmt.setString(3, member.getName());
-      pstmt.setString(4, member.getPhone_num());
-      pstmt.setString(5, member.getAddress());
-
-      result = pstmt.executeUpdate();
-    } catch (SQLException e) {
-      System.out.println("아이디 중복");
-    }
-    return result;
   }
 
   public boolean isEmpty(String value) {
